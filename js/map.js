c793ee651e49b753785f05c9ed2efc0be57556b9
VeraShishlo <shishlonik@mail.ru>
2016-02-20 18:14:23 +0300

    20.02

---
+++ js/map.js
@@ -0,0 +1,25 @@
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [59.938667, 30.323073],
+            zoom: 9
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/logo-map.png',
+            // Размеры метки.
+            iconImageSize: [180, 190],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-3, -42]
+        });
+
+    myMap.geoObjects.add(myPlacemark);
+});
---
+++ js/pop-up.js
@@ -0,0 +1,50 @@
+var link = document.querySelector(".write-me"); 
+
+var popup = document.querySelector(".pop-up-form"); 
+var close = popup.querySelector(".icon-close"); 
+
+var form = popup.querySelector("form"); 
+var login = popup.querySelector("[name=name]"); 
+var password = popup.querySelector("[name=email]"); 
+var password = popup.querySelector("[name=text]"); 
+
+var storage = localStorage.getItem("[name=name]"); 
+
+link.addEventListener("click", function(event) { 
+event.preventDefault(); 
+popup.classList.add("pop-up-form-show"); 
+
+if (storage) { 
+name.value = storage; 
+email.focus(); 
+} else { 
+name.focus(); 
+} 
+
+}); 
+
+close.addEventListener("click", function(event) { 
+event.preventDefault(); 
+popup.classList.remove("pop-up-form-show"); 
+popup.classList.remove("icon-close"); 
+}); 
+
+form.addEventListener("submit", function(event) { 
+if (!name.value || !email.value || !text.value) { 
+event.preventDefault(); 
+popup.classList.remove("pop-up-form-error"); 
+popup.offsetWidth = popup.offsetWidth; 
+popup.classList.add("pop-up-form-error"); 
+} else { 
+localStorage.setItem("name", name.value); 
+} 
+}); 
+
+window.addEventListener("keydown", function(event) { 
+if (event.keyCode === 27) { 
+if (popup.classList.contains("pop-up-form-show")) { 
+popup.classList.remove("pop-up-form-show"); 
+popup.classList.remove("pop-up-form-error"); 
+} 
+} 
+}); 
